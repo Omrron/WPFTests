5f9285ad34f98c43fad2d71dab216cf359a34ae8
Omrron <obaron4120@gmail.com>
2024-05-08 23:44:42 +0300

    Added checkable wrapper
    WIP

---
+++ WPF_Buttons/UserControls/ContentPresenters/CheckableWrapper.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Toolkit.Mvvm.Input;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPF_Buttons.UserControls.ContentPresenters
+{
+    public class CheckableWrapper : ItemsControl
+    {
+        public CheckableWrapper()
+        {
+            SetValue(CheckedItemsProperty, new List<object>());
+        }
+
+        public List<object> CheckedItems 
+        {
+            get => (List<object>)GetValue(CheckedItemsProperty); 
+            set => SetValue(CheckedItemsProperty, value);
+        }
+
+        public static readonly DependencyProperty CheckedItemsProperty =
+            DependencyProperty.Register("CheckedItems", typeof(IEnumerable<object>), typeof(CheckableWrapper));
+
+        public RelayCommand<object> m_ItemChecked => new RelayCommand<object>((_) => CheckItem(_)); 
+        public RelayCommand<object> m_ItemUnchecked => new RelayCommand<object>((_) => UncheckItem(_)); 
+
+        public void CheckItem(object item)
+        {
+            CheckedItems.Add(item);
+        }
+
+        public void UncheckItem(object item)
+        {
+            CheckedItems.Remove(item);
+        }
+    }
+}
--- WPF_Buttons/ViewModels/ButtonsTestViewModel.cs
+++ WPF_Buttons/ViewModels/ButtonsTestViewModel.cs
@@ -1,9 +1,27 @@
-﻿using System.Windows.Input;
+﻿using Microsoft.Toolkit.Mvvm.Input;
+using System.Collections.Generic;
+using WPF_Buttons.UserControls.ContentPresenters;
 
 namespace WPF_Buttons.Windows
 {
     class ButtonsTestViewModel
     {
+        CheckableWrapper CategoriesWrapper { get; set; }
+        public IEnumerable<string> Categories { get; set; }
+        public IEnumerable<object> CheckedCategoreis { get; set; }
+
+        public ButtonsTestViewModel() 
+        {
+            Categories = new List<string>() { "רכב", "רחפן", "תשתיות" };
+            CategoriesWrapper = new CheckableWrapper();
+        }
+
+        public RelayCommand hitIt => new RelayCommand(() => HitIt());
+
+        public void HitIt()
+        {
+            CheckedCategoreis = CategoriesWrapper.CheckedItems;
+        }
 
     }
 }
